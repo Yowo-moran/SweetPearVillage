642c91f1f30dd694e06c40d911bf376107ed929e
Yowo_moran <zsj2805547052@163.com>
2023-08-01 11:04:22 +0800

    uview配置

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+
+import uView from "uview-ui";
+Vue.use(uView);
+
 import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,6 +1,8 @@
 <template>
 	<view>
 		<h1>mine</h1>
+		<u-button >默认按钮</u-button>
+		<u-button type="primary">主要按钮</u-button>
 	</view>
 </template>
 
