8588cfe8711e25600212c702ab8a12f37782e473
Yowo-moran <zsj2805547052@163.com>
2023-08-02 12:08:30 +0800

    干完一半多

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,110 +1,130 @@
 <template>
-	<view class="allMine">
-		<view class="header">
-			<view class="headportrait">
-				<img src="../../static/111.jpg" alt="">
-				<view class="username" style="margin-right: 130rpx;">
-					<text style="font-weight: bold;font-size: 36rpx;color: #818258;">{{personalDetails.name}}</text>
-					<text style="margin-top: 20rpx;">{{personalDetails.gender}}</text>
-				</view>
-				<navigator url="/pages/mine/setting"><img src="../../static/设置.png" alt="" style="height: 80rpx;width: 80rpx;margin-bottom: 50rpx;"></navigator>
-			</view>
-			<view class="usermajor">
-				<text>学院： {{personalDetails.college}}</text>
-				<text style="margin-top: 10rpx;">专业： {{personalDetails.major}}</text>
-			</view>
-		</view>
-		<view class="main">
-			<navigator url="/pages/mine/myReward"><view class="options"><text>我的悬赏</text><img src="../../static/箭头.png" alt=""></view></navigator>
-			<navigator><view class="options"><text>我出售的书籍</text><img src="../../static/箭头.png" alt=""></view></navigator>
-			<navigator url="/pages/mine/myIdle"><view class="options"><text>我的闲置</text><img src="../../static/箭头.png" alt=""></view></navigator>
-			<view class="options"><text>我的帖子</text><img src="../../static/箭头.png" alt=""></view>
-		</view>
-	</view>
+  <view class="allMine">
+    <view class="header">
+      <view class="headportrait">
+        <img src="../../static/111.jpg" alt="" />
+        <view class="username" style="margin-right: 130rpx">
+          <text style="font-weight: bold; font-size: 36rpx; color: #818258">{{
+            personalDetails.name
+          }}</text>
+          <text style="margin-top: 20rpx">{{ personalDetails.gender }}</text>
+        </view>
+        <navigator url="/pages/mine/setting"
+          ><img
+            src="../../static/菜单设置.png"
+            alt=""
+            style="height: 80rpx; width: 80rpx; margin-bottom: 50rpx"
+        /></navigator>
+      </view>
+      <view class="usermajor">
+        <text>学院： {{ personalDetails.college }}</text>
+        <text style="margin-top: 10rpx"
+          >专业： {{ personalDetails.major }}</text
+        >
+      </view>
+    </view>
+    <view class="main">
+      <navigator url="/pages/mine/myReward"
+        ><view class="options"
+          ><text>我的悬赏</text><img src="../../static/箭头.png" alt="" /></view
+      ></navigator>
+      <navigator
+        ><view class="options"
+          ><text>我出售的书籍</text
+          ><img src="../../static/箭头.png" alt="" /></view
+      ></navigator>
+      <navigator url="/pages/mine/myIdle"
+        ><view class="options"
+          ><text>我的闲置</text><img src="../../static/箭头.png" alt="" /></view
+      ></navigator>
+      <view class="options"
+        ><text>我的帖子</text><img src="../../static/箭头.png" alt=""
+      /></view>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				personalDetails: {
-					name: 'Yowo_moran',
-					gender: '男',
-					college: '机械工程学院',
-					major: '机械电子工程',
-				}
-
-			};
-		}
-	}
+export default {
+  data() {
+    return {
+      personalDetails: {
+        name: "Yowo_moran",
+        gender: "男",
+        college: "机械工程学院",
+        major: "机械电子工程",
+      },
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-	.allMine {
-		// height: 950rpx;
-		background-color: #D6D7B9;
-		font-size: 28rpx;
-		color: #AEAF75;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		.header {
-			box-sizing: border-box;
-			width: 100vw;
-			height: 400rpx;
-			padding: 15rpx;
-			padding-left: 35rpx;
-			background-color: #ffffff;
-			border-radius: 0 0 80rpx 80rpx;
-			display: flex;
-			flex-direction: column;
-			.headportrait{
-				display: flex;
-				flex-direction: row;
-				justify-content: space-between;
-				align-items: center;
-				img {
-					width: 170rpx;
-					height: 170rpx;
-					border-radius: 50%;
-					margin: 15rpx;
-				}
-				.username{
-					display: flex;
-					flex-direction: column;
-					justify-content: space-around;
-				}
-			}
-			.usermajor{
-				display: flex;
-				flex-direction: column;
-				margin: 15rpx;
-			}
-		}
-		.main{
-			box-sizing: border-box;
-			width: 90vw;
-			height: 64vh;
-			margin: 5vw;
-			.options{
-				width: 100%;
-				height: 120rpx;
-				margin-bottom: 20rpx;
-				background-color: #C4DDAF;
-				display: flex;
-				justify-content: space-between;
-				align-items: center;
-				text{
-					color: #8DA56F;
-					font-size: 42rpx;
-					margin-left: 25rpx;
-				}
-				img{
-					width: 50rpx;
-					height: 50rpx;
-					margin-right: 40rpx;
-				}
-			}
-		}
-	}
+.allMine {
+  // height: 950rpx;
+  background-color: #d6d7b9;
+  font-size: 28rpx;
+  color: #aeaf75;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .header {
+    box-sizing: border-box;
+    width: 100vw;
+    height: 400rpx;
+    padding: 15rpx;
+    padding-left: 35rpx;
+    background-color: #ffffff;
+    border-radius: 0 0 80rpx 80rpx;
+    display: flex;
+    flex-direction: column;
+    .headportrait {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      img {
+        width: 170rpx;
+        height: 170rpx;
+        border-radius: 50%;
+        margin: 15rpx;
+      }
+      .username {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+      }
+    }
+    .usermajor {
+      display: flex;
+      flex-direction: column;
+      margin: 15rpx;
+    }
+  }
+  .main {
+    box-sizing: border-box;
+    width: 90vw;
+    height: 64vh;
+    margin: 5vw;
+    .options {
+      width: 100%;
+      height: 120rpx;
+      margin-bottom: 20rpx;
+      background-color: #c4ddaf;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      text {
+        color: #8da56f;
+        font-size: 42rpx;
+        margin-left: 25rpx;
+      }
+      img {
+        width: 50rpx;
+        height: 50rpx;
+        margin-right: 40rpx;
+      }
+    }
+  }
+}
 </style>
--- pages/mine/myIdle.vue
+++ pages/mine/myIdle.vue
@@ -1,109 +1,111 @@
 <template>
-	<view class="allIdle">
-		<view v-for="(item,index) in lists" class="listItem">
-			<view class="card">
-				<img src="../../static/111.jpg" alt="">
-				<view class="content">
-					<text>{{item.article}}</text>
-					<view class="user">
-						<img src="../../static/111.jpg" alt="">
-						<text>{{item.name}}</text>
-					</view>
-				</view>
-				<view class="threeButton">
-					<button>删除</button>
-					<button>编辑</button>
-					<button>已出</button>
-					<text>￥{{item.price}}</text>
-				</view>
-			</view>
-		</view>
+  <view class="allIdle">
+    <view v-for="(item, index) in lists" :key="index" class="listItem">
+      <view class="card">
+        <img src="../../static/111.jpg" alt="" />
+        <view class="content">
+          <text>{{ item.article }}</text>
+          <view class="user">
+            <img src="../../static/111.jpg" alt="" />
+            <text>{{ item.name }}</text>
+          </view>
+        </view>
+        <view class="threeButton">
+          <button>删除</button>
+          <button>编辑</button>
+          <button>已出</button>
+          <text>￥{{ item.price }}</text>
+        </view>
+      </view>
+    </view>
+	<view class="listItem">
 	</view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				lists:[
-					{	
-						img:'',
-						article:'啊啊啊！怎么会有人看错文档？我是呆逼！！',
-						price:'250',
-						name:'moran',
-					},
-					{
-						img:'',
-						article:'啊啊啊！怎么会有人看错文档？我是呆逼！！',
-						price:'250',
-						name:'moran',
-					},
-				]
-			};
-		}
-	}
+export default {
+  data() {
+    return {
+      lists: [
+        {
+          img: "",
+          article: "啊啊啊！怎么会有人看错文档？我是呆逼！！",
+          price: "250",
+          name: "moran",
+        },
+        {
+          img: "",
+          article: "啊啊啊！怎么会有人看错文档？我是呆逼！！",
+          price: "250",
+          name: "moran",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-.allIdle{
-	width: 100vw;
-	height: 100vh;
-	background-color: #D6D7B9;
-	.listItem{
-		height: 250rpx;
-		padding:15rpx 20rpx;
-		background-color: #D6D7B9;
-		.card{
-			height: 175rpx;
-			padding: 25rpx;
-			background-color: #fff;
-			box-shadow: 0 2px 4px rgba(0, 0, 0, .32), 0 0 6px rgba(0, 0, 0, .34);
-			display: flex;
-			img{
-				width: 175rpx;
-				height: 175rpx;
-				border-radius: 30rpx;
-			}
-			.content{
-				font-size: 24rpx;
-				margin-left: 20rpx;
-				width: 380rpx;
-				height: 180rpx;
-				display: flex;
-				flex-direction: column;
-				justify-content: space-between;
-				.user{
-					display: flex;
-					align-items: center;
-					font-size: 32rpx;
-					text{
-						margin-left: 10rpx;
-					}
-					img{
-						width: 50rpx;
-						height: 50rpx;
-						border-radius: 50%;
-					}
-				}
-			}
-			.threeButton{
-				button{
-					font-size: 28rpx;
-					padding: 0;
-					width: 80rpx;
-					height: 40rpx;
-					line-height: 40rpx;
-					color: #7C7D54;
-					margin-bottom: 10rpx;
-					background-color: #C1C4A6;
-				}
-				text{
-					font-size: 24rpx;
-					font-weight: bold;
-					color: #BE978F;
-				}
-			}
-		}
-	}
+.allIdle {
+  width: 100vw;
+  height: 100vh;
+  background-color: #d6d7b9;
+  .listItem {
+    height: 230rpx;
+    padding: 30rpx 20rpx 0 20rpx;
+    background-color: #d6d7b9;
+    .card {
+      height: 175rpx;
+      padding: 25rpx;
+      background-color: #fff;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.32), 0 0 6px rgba(0, 0, 0, 0.34);
+      display: flex;
+      img {
+        width: 175rpx;
+        height: 175rpx;
+        border-radius: 30rpx;
+      }
+      .content {
+        font-size: 24rpx;
+        margin-left: 20rpx;
+        width: 380rpx;
+        height: 180rpx;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .user {
+          display: flex;
+          align-items: center;
+          font-size: 32rpx;
+          text {
+            margin-left: 10rpx;
+          }
+          img {
+            width: 50rpx;
+            height: 50rpx;
+            border-radius: 50%;
+          }
+        }
+      }
+      .threeButton {
+        button {
+          font-size: 28rpx;
+          padding: 0;
+          width: 80rpx;
+          height: 40rpx;
+          line-height: 40rpx;
+          color: #7c7d54;
+          margin-bottom: 10rpx;
+          background-color: #c1c4a6;
+        }
+        text {
+          font-size: 24rpx;
+          font-weight: bold;
+          color: #be978f;
+        }
+      }
+    }
+  }
 }
 </style>
