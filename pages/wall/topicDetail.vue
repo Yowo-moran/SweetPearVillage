1b52c50327d1bfbf3bbd0b49a4c5bfff02e600bb
AwayHoper <1508262532@qq.com>
2023-08-03 19:18:52 +0800

    墙墙静态完成

---
+++ pages/wall/topicDetail.vue
@@ -0,0 +1,43 @@
+<template>
+    <view class="main">
+        <topic></topic>
+        <view class="commentArea">
+            <comment-list></comment-list>
+        </view>
+    </view>
+</template>
+
+<script>
+import commentList from './commentList.vue';
+import topic from './topic.vue';
+export default {
+    components:{ topic , commentList },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    .main {
+        margin: 0 auto;
+        // border: 2px solid red;
+        width: 740rpx;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .commentArea {
+            // border: 2px solid blue;
+            background-color: #E9E9C4;
+            border-radius: 15rpx;
+            width: 650rpx;
+            // height: 500rpx;
+            display: flex;
+            align-items: center;
+            justify-content:space-around;
+            padding-top: 20rpx;
+        }
+    }
+</style>
