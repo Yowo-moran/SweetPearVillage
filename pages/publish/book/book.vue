3fc92b4253d4f2e85901114d28d6cb7cedfdf911
wwwwwst <804927063@qq.com>
2023-08-03 15:44:10 +0800

    commit

---
+++ pages/publish/book/book.vue
@@ -0,0 +1,226 @@
+<template>
+	<view>
+		<u-navbar leftText="书籍"
+		:autoBack="true" 
+		:bgColor="bgColor" 
+		placeholder> </u-navbar>
+		<u-form :labelStyle="labelStyle" :model="model1">
+			<u-form-item label="书名:" prop="userInfo.name">
+				<input type="text" 
+				placeholder="请输入书名" 
+				v-model="model1.userInfo.name">
+			</u-form-item>
+			
+			<u-form-item label="价格:" prop="userInfo.price">
+				<input type="number" 
+				placeholder="请输入价钱"
+				v-model="model1.userInfo.price">
+			</u-form-item>
+			
+			<u-form-item label="学院:"
+			@click="showCollege = true"
+			prop="userInfo.college"
+			>
+				<u--input
+					v-model="model1.userInfo.college"
+					placeholder="请选择学院"
+					border="none"
+				></u--input>
+				<u-icon
+					slot="right"
+					name="arrow-right"
+				></u-icon>
+			</u-form-item>
+			
+			<u-form-item label="专业"
+			@click="showMajor = true"
+			prop="userInfo.major">
+				<u--input
+					v-model="model1.userInfo.major"
+					placeholder="请选择专业"
+					border="none"
+				></u--input>
+				<u-icon
+					slot="right"
+					name="arrow-right"
+				></u-icon>
+			</u-form-item>
+			
+			
+			<u-form-item label="年级"
+			@click="showGrade = true"
+			prop="userInfo.grade">
+				<u--input
+					v-model="model1.userInfo.grade"
+					placeholder="请选择年级"
+					border="none"
+				></u--input>
+				<u-icon
+					slot="right"
+					name="arrow-right"
+				></u-icon>
+			</u-form-item>
+		</u-form>
+		<u-action-sheet
+			:show="showCollege"
+			:actions="college"
+			title="请选择学院"		
+			@close="showCollege = false"
+			@select="collegeSelect"
+				>
+		</u-action-sheet>
+		<u-action-sheet
+			:show="showMajor"
+			:actions="major"
+			title="请选择专业"		
+			@close="showMajor = false"
+			@select="majorSelect">
+		</u-action-sheet>
+		<u-action-sheet
+			:show="showGrade"
+			:actions="grade"
+			title="请选择年级"		
+			@close="showGrade = false"
+			@select="gradeSelect">
+		</u-action-sheet>
+		<u-upload
+				:fileList="fileList1"
+				@afterRead="afterRead"
+				@delete="deletePic"
+				name="1"
+				multiple
+				:maxCount="10"
+			></u-upload>
+			<view class="bottom">
+				<button>发布</button>
+			</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				bgColor:"#d6d7b9",
+				showCollege:false,
+				showMajor:false,
+				showGrade:false,
+				labelStyle:{
+					color:'#626341',
+				},
+				model1: {
+					userInfo: {
+						name:'',
+						price:'',
+						college: '',
+						major:'',
+						grade:'',
+					},
+				},
+				college:[{
+					name:"计算机技术与工程学院"
+				},
+				{
+					name:"机械工程学院"
+				}
+				],
+				major:[{
+					name:"计算机科学与技术"
+				},
+				{
+					name:"大数据"
+				}
+				],
+				grade:[{
+					name:"大一"
+				},
+				{
+					name:"大二"
+				},
+				{
+					name:"大三"
+				},
+				{
+					name:"大四"
+				}
+				],
+				fileList1: [],
+			};
+		},
+		methods:{
+			collegeSelect(e){
+				this.model1.userInfo.college = e.name
+			},
+			majorSelect(e){
+				this.model1.userInfo.major = e.name
+			},
+			gradeSelect(e){
+				this.model1.userInfo.grade= e.name
+			},
+			// 删除图片
+			deletePic(event) {
+				this[`fileList${event.name}`].splice(event.index, 1)
+			},
+			// 新增图片
+			async afterRead(event) {
+				// 当设置 multiple 为 true 时, file 为数组格式，否则为对象格式
+				let lists = [].concat(event.file)
+				let fileListLen = this[`fileList${event.name}`].length
+				lists.map((item) => {
+					this[`fileList${event.name}`].push({
+						...item,
+						status: 'uploading',
+						message: '上传中'
+					})
+				})
+				for (let i = 0; i < lists.length; i++) {
+					const result = await this.uploadFilePromise(lists[i].url)
+					let item = this[`fileList${event.name}`][fileListLen]
+					this[`fileList${event.name}`].splice(fileListLen, 1, Object.assign(item, {
+					status: 'success',
+					message: '',
+					url: result
+				}))
+				fileListLen++
+				}
+				},
+				
+			uploadFilePromise(url) {
+				return new Promise((resolve, reject) => {
+					let a = uni.uploadFile({
+						url: 'http://192.168.2.21:7001/upload', // 仅为示例，非真实的接口地址
+						filePath: url,
+						name: 'file',
+						formData: {
+							user: 'test'
+						},
+						success: (res) => {
+							setTimeout(() => {
+								resolve(res.data.data)
+							}, 1000)
+						}
+					});
+				})
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+.u-form-item{
+	padding:0px 10px;
+}
+.u-upload{
+	padding-left: 20px;
+	padding-top: 10px;
+}
+button{
+	
+	background-color:rgb(214,215,185);
+	width: 150rpx;
+	line-height: 80rpx;
+	fontsize:10px;
+	color: rgb(124,125,82);
+		
+	}
+</style>
---
+++ pages/publish/reward/reward.vue
@@ -0,0 +1,140 @@
+<template>
+	<view class="body">
+		
+		<u-navbar leftText="悬赏" 
+		:autoBack="true" 
+		:bgColor="bgColor" 
+		placeholder> </u-navbar>
+		
+		<u-form :labelStyle="labelStyle" :model="model1" labelWidth="100">
+			<u-form-item prop="userInfo.content">
+				<span >悬赏内容：</span>
+				<u--textarea v-model="model1.userInfo.content" 
+				placeholder="请输入悬赏内容" 
+				count 
+				border="none"
+				></u--textarea>
+			</u-form-item>
+			
+			
+			<u-form-item label="赏金:" prop="userInfo.price">
+				<u--input placeholder="请输入金额" 
+				type="number"
+				border="none"
+				v-model="model1.userInfo.price"></u--input>
+			</u-form-item>
+			
+			<u-form-item label="地点:"
+			@click="showPlace = true"
+			prop="userInfo.place"
+			
+			>
+				<u--input
+					v-model="model1.userInfo.place"
+					placeholder="请选择地点"
+					border="none"
+				></u--input>
+				<u-icon
+					slot="right"
+					name="arrow-down"
+				></u-icon>
+			</u-form-item>
+			
+			<u-form-item label="收货地址:" prop="userInfo.getPlace">
+				<u--input placeholder="请输入收货地址"
+				border="none"
+				v-model="model1.userInfo.getPlace">
+				</u--input>
+			</u-form-item> 
+		</u-form>
+		
+		<!-- <u-form>
+			<u-form-item>
+				<span >悬赏内容：</span>
+				<u--textarea 
+				placeholder="请输入悬赏内容" 
+				count height="100px"
+				></u--textarea>
+			</u-form-item>
+			<u-form-item>
+				<span>赏金:</span>
+				<u--input placeholder="请输入金额" type="number"></u--input>
+			</u-form-item>
+			<u-form-item>
+				<span>地点:</span>
+				<u--input type="text" placeholder="请输入地点,可选择" ></u--input>
+				
+			</u-form-item>
+			<u-form-item>
+				<span>收货地址:</span>
+				<u--input placeholder="请输入收货地址"></u--input>
+			</u-form-item>
+		</u-form> -->	
+			
+			
+			
+		
+		
+		<view class="bottom">
+			<button>发布</button>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				bgColor:"#d6d7b9",
+				showPlace:false,
+				labelStyle:{
+					color:'#626341',
+					
+				},
+				model1: {
+					userInfo: {
+						content:'',
+						price:'',
+						place: '',
+						getPlace:'',
+						
+					},
+				},
+			};
+		},
+		 methods:{
+		      
+		    }
+	}
+</script>
+
+<style lang="scss">
+.bottom{
+	display: flex;
+	
+	// flex-direction: row-reverse;
+	// justify-content: flex-end;
+	padding-left: 500rpx;
+	
+}
+.u-form-item{
+	padding:0px 10px;
+	span{
+		color: #626341;
+		font-size: 15px;
+	}
+}
+button{
+	
+	background-color:rgb(214,215,185);
+	width: 150rpx;
+	line-height: 80rpx;
+	fontsize:10px;
+	color: rgb(124,125,82);
+		
+	}
+// .u-form{
+	
+// }
+</style>
